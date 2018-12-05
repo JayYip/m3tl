a765c04a690529969a54bb1bdce542ed40b56f45
JayYip <junpang.yip@gmail.com>
2018-12-05 16:53:14 +0800

    [feature] Add _apply_sparse to optimizer

--- src/optimizer.py
+++ src/optimizer.py
@@ -11,6 +11,8 @@ from tensorflow.python.framework import ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.training import optimizer
+from tensorflow.python.ops import state_ops
+from tensorflow.python.ops import resource_variable_ops
 
 
 class AdamWeightDecayOptimizer(optimizer.Optimizer):
@@ -113,6 +115,58 @@ class AdamWeightDecayOptimizer(optimizer.Optimizer):
         return control_flow_ops.group(*[var.assign(next_param),
                                         m.assign(next_m),
                                         v.assign(next_v)])
+
+    def _apply_sparse_shared(self, grad, var, indices, scatter_add):
+        learning_rate_t = math_ops.cast(
+            self.learning_rate_t, var.dtype.base_dtype)
+        beta_1_t = math_ops.cast(self.beta_1_t, var.dtype.base_dtype)
+        beta_2_t = math_ops.cast(self.beta_2_t, var.dtype.base_dtype)
+        epsilon_t = math_ops.cast(self.epsilon_t, var.dtype.base_dtype)
+        weight_decay_rate_t = math_ops.cast(
+            self.weight_decay_rate_t, var.dtype.base_dtype)
+
+        m = self.get_slot(var, 'm')
+        v = self.get_slot(var, 'v')
+
+        m_t = state_ops.assign(m, m * beta_1_t,
+                               use_locking=self._use_locking)
+
+        m_scaled_g_values = grad * (1 - beta_1_t)
+        with ops.control_dependencies([m_t]):
+            m_t = scatter_add(m, indices, m_scaled_g_values)
+
+        v_scaled_g_values = (grad * grad) * (1 - beta_2_t)
+        v_t = state_ops.assign(v, v * beta_2_t, use_locking=self._use_locking)
+        with ops.control_dependencies([v_t]):
+            v_t = scatter_add(v, indices, v_scaled_g_values)
+
+        update = m_t / (math_ops.sqrt(v_t) + epsilon_t)
+
+        if self._do_use_weight_decay(var.name):
+            update += weight_decay_rate_t * var
+
+        update_with_lr = learning_rate_t * update
+
+        var_update = state_ops.assign_sub(var,
+                                          update_with_lr,
+                                          use_locking=self._use_locking)
+        return control_flow_ops.group(*[var_update, m_t, v_t])
+
+    def _apply_sparse(self, grad, var):
+        return self._apply_sparse_shared(
+            grad.values, var, grad.indices,
+            lambda x, i, v: state_ops.scatter_add(  # pylint: disable=g-long-lambda
+                x, i, v, use_locking=self._use_locking))
+
+    def _resource_scatter_add(self, x, i, v):
+        with ops.control_dependencies(
+            [resource_variable_ops.resource_scatter_add(
+                x.handle, i, v)]):
+            return x.value()
+
+    def _resource_apply_sparse(self, grad, var, indices):
+        return self._apply_sparse_shared(
+            grad, var, indices, self._resource_scatter_add)
 
     def _do_use_weight_decay(self, param_name):
         """Whether to use L2 weight decay for `param_name`."""
