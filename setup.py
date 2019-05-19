fe905131287d2e218630e349efa13bbb3e2b30e3
Jay Yip <junpang.yip@gmail.com>
2019-05-19 05:21:42 +0000

    [feature] Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
+
+setup(
+    name='bert_multitask_learning',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/JayYip/bert-multitask-learning',
+    license='MIT',
+    author='Jay Yip',
+    author_email='junpang.yip@gmail.com',
+    description='BERT for Multi-task Learning',
+    long_description='None so far',
+    long_description_content_taype='text/markdown',
+    python_requires='>=3.5.0',
+    install_requires=install_requires,
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
