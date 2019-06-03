f56fd0e1c79f66f75745d740766e30d63e697f17
Jay Yip <junpang.yip@gmail.com>
2019-06-03 22:02:15 +0800

    [version] Update to v0.2.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setup(
     name='bert_multitask_learning',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     url='https://github.com/JayYip/bert-multitask-learning',
     license='MIT',
