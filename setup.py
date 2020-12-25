866091c455958e6f623d99db4303c3fc43f62d74
JayYip <junpang.yip@gmail.com>
2020-12-25 16:42:09 +0800

    version: version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setup(
     name='bert_multitask_learning',
-    version='0.6.0',
+    version='0.6.1a',
     packages=find_packages(),
     url='https://github.com/JayYip/bert-multitask-learning',
     license='MIT',
