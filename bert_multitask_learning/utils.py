bca14e3e6547978d910dae2a88da3942679112bc
JayYip <junpang.yip@gmail.com>
2021-01-01 15:52:27 +0800

    clean: remove redundant code

--- bert_multitask_learning/utils.py
+++ bert_multitask_learning/utils.py
@@ -377,3 +377,21 @@ def get_transformer_main_model(model, key='embeddings'):
 def get_embedding_table_from_model(model):
     base_model = get_transformer_main_model(model)
     return base_model.embeddings.word_embeddings
+
+
+def gather_indexes(sequence_tensor, positions):
+    """Gathers the vectors at the specific positions over a minibatch."""
+    sequence_shape = modeling.get_shape_list(sequence_tensor, expected_rank=3)
+    batch_size = sequence_shape[0]
+    seq_length = sequence_shape[1]
+    width = sequence_shape[2]
+
+    flat_offsets = tf.reshape(
+        tf.range(0, batch_size, dtype=tf.int32) * seq_length, [-1, 1])
+    flat_offsets = tf.cast(flat_offsets, tf.int64)
+    flat_positions = tf.reshape(positions + flat_offsets, [-1])
+    flat_sequence_tensor = tf.reshape(sequence_tensor,
+                                      [batch_size * seq_length, width])
+    # https://github.com/tensorflow/tensorflow/issues/36236
+    output_tensor = tf.gather(flat_sequence_tensor*1, flat_positions)
+    return output_tensor
