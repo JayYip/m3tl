f91238ad7614eed0b99b465102a24c8bc45049ee
JayYip <junpang.yip@gmail.com>
2021-01-04 16:24:13 +0800

    minor: convert

--- bert_multitask_learning/_nbdev.py
+++ bert_multitask_learning/_nbdev.py
@@ -5,9 +5,11 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"BaseParams": "00_params.ipynb",
          "CRFParams": "00_params.ipynb",
          "StaticBatchParams": "00_params.ipynb",
-         "DynamicBatchSizeParams": "00_params.ipynb"}
+         "DynamicBatchSizeParams": "00_params.ipynb",
+         "preprocessing_fn": "01_preproc_decorator.ipynb"}
 
-modules = ["params.py"]
+modules = ["params.py",
+           "preproc_decorator.py"]
 
 doc_url = "https://JayYip.github.io/bert_multitask_learning/"
 
